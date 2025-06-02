3b0bcfb3ea7f9d3cfa84910f7cf036bfbc71e328
StounchVicinity65 <wafashafowan@gmail.com>
2025-06-02 16:37:04 +0600

    Update fission.js

--- js/fission.js
+++ js/fission.js
@@ -1,19 +1,24 @@
 let core, fragments = [];
+let fissionTriggered = false;
 
 function setup() {
   createCanvas(windowWidth - 240, windowHeight);
-  core = { x: width/2, y: height/2, exploded: false };
+  core = { x: width / 2, y: height / 2, exploded: false };
 }
 
 function simulateFission() {
-  core.exploded = true;
-  for (let i = 0; i < 10; i++) {
-    fragments.push({
-      x: core.x,
-      y: core.y,
-      vx: random(-3, 3),
-      vy: random(-3, 3)
-    });
+  fissionTriggered = true;
+  if (!core.exploded) {
+    for (let i = 0; i < 12; i++) {
+      fragments.push({
+        x: core.x,
+        y: core.y,
+        vx: random(-4, 4),
+        vy: random(-4, 4),
+        size: random(5, 15)
+      });
+    }
+    core.exploded = true;
   }
 }
 
@@ -22,13 +27,13 @@ function draw() {
 
   if (!core.exploded) {
     fill(0);
-    ellipse(core.x, core.y, 50);
+    ellipse(core.x, core.y, 60);
   } else {
     fill(255, 0, 0);
     for (let frag of fragments) {
       frag.x += frag.vx;
       frag.y += frag.vy;
-      ellipse(frag.x, frag.y, 10);
+      ellipse(frag.x, frag.y, frag.size);
     }
   }
 }
