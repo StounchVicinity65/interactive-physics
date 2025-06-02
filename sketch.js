83879f4f7e534dbc29d2b975d6b2f3f3cd7e2ec8
StounchVicinity65 <wafashafowan@gmail.com>
2025-06-02 10:29:49 +0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,10 +2,7 @@ function setup() {
   createCanvas(600, 400);
   background(0);
 }
-
-function draw() {
-  background(0);
-  fill(255);
-  textSize(24);
-  text("Hello Physics!", 200, 200);
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
+
