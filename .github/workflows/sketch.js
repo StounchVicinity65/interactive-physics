a58d1d3fbfb5fc47c763db16665004637071c657
StounchVicinity65 <wafashafowan@gmail.com>
2025-06-02 10:02:43 +0600

    Add files via upload

---
+++ .github/workflows/sketch.js
@@ -0,0 +1,12 @@
+```javascript
+function setup() {
+  createCanvas(600, 400);
+  background(0);
+}
+
+function draw() {
+  background(0);
+  fill(255);
+  textSize(24);
+  text("Hello Physics!", 200, 200);
+}
