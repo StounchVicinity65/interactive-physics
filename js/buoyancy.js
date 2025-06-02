72dc6bba496e1c399ef85fbcf1ecf8d58a47baf2
StounchVicinity65 <wafashafowan@gmail.com>
2025-06-02 16:00:02 +0600

    Update buoyancy.js

--- js/buoyancy.js
+++ js/buoyancy.js
@@ -1,32 +1,41 @@
 let weightInput, densityInput;
-let boxY = 300, velocity = 0;
+let boxY, velocity, isSimulating = false;
 
 function setup() {
   createCanvas(windowWidth - 240, windowHeight);
   weightInput = document.getElementById('weight');
   densityInput = document.getElementById('density');
+  resetSimulation();
 }
 
 function simulateBuoyancy() {
-  boxY = 300;
+  resetSimulation();
+  isSimulating = true;
+}
+
+function resetSimulation() {
+  boxY = height / 2 + 50;
   velocity = 0;
+  isSimulating = false;
 }
 
 function draw() {
   background(200, 230, 255);
   fill(0, 100, 255, 150);
-  rect(0, height/2, width, height/2);
+  rect(0, height / 2, width, height / 2); // water
 
-  let weight = float(weightInput.value);
-  let density = float(densityInput.value);
-  let buoyantForce = density * 9.8;
-  let netForce = buoyantForce - weight;
+  if (isSimulating) {
+    let weight = float(weightInput.value);
+    let density = float(densityInput.value);
+    let buoyantForce = density * 9.8;
+    let netForce = buoyantForce - weight;
 
-  velocity += netForce * 0.1;
-  boxY += velocity;
-
-  boxY = constrain(boxY, height/2, height - 50);
+    velocity += netForce * 0.1;
+    boxY += velocity;
+    boxY = constrain(boxY, height / 2, height - 50);
+  }
 
   fill(255);
-  rect(width/2 - 25, boxY, 50, 50);
+  rect(width / 2 - 25, boxY, 50, 50);
 }
+
