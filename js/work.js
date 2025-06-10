e20878a070114e3e55514ac81a7f4e6e319e1bb8
StounchVicinity65 <wafashafowan@gmail.com>
2025-06-10 15:40:22 +0600

    Update work.js

--- js/work.js
+++ js/work.js
@@ -1,20 +1,22 @@
-let forceInput, displacementInput, angleInput
-let work = null;
+ let forceInput, displacementInput, angleInput
+ let work = null;
 
-function setup() {
-  createCanvas(windowWidth - 240, windowHeight);
-  forceInput = document.getElementById('force');
-  displacementInput = document.getElementById('displacement');
-  angleInput = document.getElementById('angle');
-}
+ function setup() {
+   createCanvas(windowWidth - 240, windowHeight);
+   forceInput = document.getElementById('force');
+   displacementInput = document.getElementById('displacement');
+   angleInput = document.getElementById('angle');
+ }
 
-function draw() {
-  background(240);
-  displacement += 1;
-  work = force * displacement;
+ function draw() {
+   background(240);
+   displacement += 1;
+   work = force * displacement;
 
-  fill(255, 200, 0);
-  rect(displacement, height / 2, 50, 50);
-
-  if (displacement > width) displacement = 0;
+   fill(255, 200, 0);
+   rect(displacement, height / 2, 50, 50);
+ 
+    if (displacement > width) displacement = 0;
+    }
+  }
 }
